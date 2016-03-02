00d5f011e22d2c1a000b83134855854ee0b11555
Rory Brosnan <rbrosnan@gmail.com>
2016-03-01 23:56:31 -0500

    Step 3 Complete

--- client/app.ts
+++ client/app.ts
@@ -1,7 +1,9 @@
-import {Component, View} from 'angular2/core';
+import {Component, View, NgZone} from 'angular2/core';
  
-import {bootstrap} from 'angular2/platform/browser';
+import {bootstrap} from 'angular2-meteor';
  
+import {Parties} from 'collections/parties';
+
 @Component({
     selector: 'app'
 })
@@ -9,23 +11,10 @@ import {bootstrap} from 'angular2/platform/browser';
     templateUrl: 'client/app.html'
 })
 class Socially {
-    parties: Array<Object>;
+    parties: Mongo.Cursor<Object>;
  
     constructor () {
-        this.parties = [
-          {'name': 'Dubstep-Free Zone',
-           'description': 'Can we please just for an evening not listen to dubstep.',
-           'location': 'Palo Alto'
-          },
-          {'name': 'All dubstep all the time',
-           'description': 'Get it on!',
-           'location': 'Palo Alto'
-          },
-          {'name': 'Savage lounging',
-           'description': 'Leisure suit required. And only fiercest manners.',
-           'location': 'San Francisco'
-          }
-        ];
+        this.parties = Parties.find();
     }
 }
  
